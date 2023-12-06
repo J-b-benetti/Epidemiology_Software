5891e9ac5c7e0330eaf397c240ace210c55f8644
J-b-benetti <j.b.benetti81@gmail.com>
2023-12-06 19:28:47 +0100

    Commit de calcul.py pour enquête de type CAS-Témoins

--- backend/calcul.py
+++ backend/calcul.py
@@ -59,16 +59,30 @@ def r():
     return round(m1() / total(), 2)
 
 
-def odd_ratio():
+def f0():
+    return round(x10 / m0(), 2)
+
+def f1():
+    return round(x11 / m1(), 2)
+
+def f():
+    return round(n1() / total(), 2)
+
+def odd_ratio_d_e():
     return round((x00 * x11) / (x10 * x01), 2)
 
+def odd_ratio_e_d():
+    return round((x00 * x11) / (x10 * x01), 2)
 
 def var_r1_r0():
     calcul = r() * (1 - r()) * (1 / n0() + 1 / n1())
     return calcul
 
+def var_f1_f0():
+    calcul = f() * (1 - f()) * (1 / m0() + 1 / m1())
+    return calcul
 
-def test_hypothese():
+def test_hypothese_d_e():
     calcul = abs((r1() - r0()) / sqrt(var_r1_r0()))
     if calcul > 2.58:
         return "L'hypothèse de départ H0 est rejetée. E et D ne sont pas indépendant sur l'intervalle de confiance de " \
@@ -79,21 +93,40 @@ def test_hypothese():
     else:
         return "L'hypothèse de départ H0 n'est pas rejetée. E et D sont indépendants", calcul
 
+def test_hypothese_e_d():
+    calcul = abs((f1() - f0()) / sqrt(var_f1_f0()))
+    if calcul > 2.58:
+        return "L'hypothèse de départ H0 est rejetée. E et D ne sont pas indépendant sur l'intervalle de confiance de " \
+               "98 % ", calcul
+    elif calcul > 1.96:
+        return "L'hypothèse de départ H0 est rejetée. E et D ne sont pas indépendant sur l'intervalle de confiance de " \
+               "95 % ", calcul
+    else:
+        return "L'hypothèse de départ H0 n'est pas rejetée. E et D sont indépendants", calcul
 
-def nu():
-    calcul = log(odd_ratio())
+def nu_d_e():
+    calcul = log(odd_ratio_d_e())
     return round(calcul, 2)
 
+def nu_e_d():
+    calcul = log(odd_ratio_e_d())
+    return round(calcul, 2)
 
-def var_nu():
+def var_nu_d_e():
     return round((1 / x00) + (1 / x01) + (1 / x10) + (1 / x11), 2)
 
+def var_nu_e_d():
+    return round((1 / x00) + (1 / x01) + (1 / x10) + (1 / x11), 2)
 
-def intervalle_confiance():
-    val_min = exp(nu() - 1.96*sqrt(var_nu()))
-    val_max = exp(nu() + 1.96*sqrt(var_nu()))
+def intervalle_confiance_d_e():
+    val_min = exp(nu_d_e() - 1.96*sqrt(var_nu_d_e()))
+    val_max = exp(nu_d_e() + 1.96*sqrt(var_nu_d_e()))
     return [round(val_min, 2), round(val_max, 2)]
 
+def intervalle_confiance_e_d():
+    val_min = exp(nu_e_d() - 1.96*sqrt(var_nu_e_d()))
+    val_max = exp(nu_d_e() + 1.96*sqrt(var_nu_e_d()))
+    return [round(val_min, 2), round(val_max, 2)]
 
 print("X0,0: ", x00, "X0,1: ", x01, "X1,0: ", x10, "X1,1: ", x11)
 print("n0: ", n0(), "n1: ", n1(), "m0: ", m0(), "m1: ", m1())
@@ -102,8 +135,8 @@ print("Total: ", total())
 print("R0: ", r0(), "R1: ", r1())
 print("R: ", r())
 
-print("Odd Ratio: ", odd_ratio())
-print("Hypothèse: ", test_hypothese())
-print("Nu: ", nu())
-print("Var(nu): ", var_nu())
-print("IC(1 - alpha): ", intervalle_confiance())
+print("Odd Ratio: ", odd_ratio_d_e())
+print("Hypothèse: ", test_hypothese_d_e())
+print("Nu: ", nu_d_e())
+print("Var(nu): ", var_nu_d_e())
+print("IC(1 - alpha): ", intervalle_confiance_d_e())
