6b633c1216004c34efb48e0a3b37140c422098b0
J-b-benetti <j.b.benetti81@gmail.com>
2024-01-30 17:57:37 +0100

    Code cleared !

--- main/calcul.py
+++ main/calcul.py
@@ -1,24 +1,5 @@
 from math import *
 
-# x00 = None
-# x01 = None
-# x10 = None
-# x11 = None
-
-'''while x00 is None or x01 is None or x10 is None or x11 is None:
-    try:
-        if x00 is None:
-            x00 = int(input("Entrer X0,0: "))
-        if x01 is None:
-            x01 = int(input("Entrer X0,1: "))
-        if x10 is None:
-            x10 = int(input("Entrer X1,0: "))
-        if x11 is None:
-            x11 = int(input("Entrer X1,1: "))
-    except ValueError:
-        print("Veuillez entrer un nombre entier valide.")
-'''
-
 def n0(x00, x01):
     return x00 + x01
 
@@ -115,33 +96,3 @@ def intervalle_confiance_e_d(x00, x11, x10, x01):
     val_min = exp(nu_d_e(x00, x11, x10, x01) - 1.96*sqrt(var_nu_d_e(x00, x11, x10, x01)))
     val_max = exp(nu_d_e(x00, x11, x10, x01) + 1.96*sqrt(var_nu_d_e(x00, x01, x10, x11)))
     return [round(val_min, 2), round(val_max, 2)]
-
-
-
-'''
-print("X0,0: ", x00, "\nX0,1: ", x01, "\nX1,0: ", x10, "\nX1,1: ", x11)
-print()
-print("n0: ", n0(), "\nn1: ", n1(), "\nm0: ", m0(), "\nm1: ", m1())
-print("Total: ", total())
-print()
-print("R0: ", r0(), "R1: ", r1())
-print()
-print("R: ", r())
-
-print("Odd Ratio D/E: ", odd_ratio_d_e())
-print("Hypothèse: ", test_hypothese_d_e())
-print("Nu D/E: ", nu_d_e())
-print("Var(nu D/E): ", var_nu_d_e())
-print("IC(1 - alpha) (D/E): ", intervalle_confiance_d_e())
-
-print("-------------------------------")
-
-print("F0: ", f0(), "F1: ", f1())
-print("F: ", f())
-
-print("Odd Ratio E/D: ", odd_ratio_e_d())
-print("Hypothèse: ", test_hypothese_e_d())
-print("Nu E/D: ", nu_e_d())
-print("Var(nu E/D): ", var_nu_e_d())
-print("IC(1 - alpha) (E/D): ", intervalle_confiance_e_d())
-'''
